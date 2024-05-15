b3b028c72e04109f25ed62bddcbfe7dbd09e5e15
Aleksandar Pavicevic <aleksandar@DESKTOP-TAFAP9P.localdomain>
2024-05-15 20:19:13 +0200

    got around to pushing shifts to localStorage on shift end

--- script.js
+++ script.js
@@ -1,28 +1,61 @@
 const punchIn = document.querySelector("#punch-in");
 const punchOut = document.querySelector("#punch-out");
+const reset = document.querySelector("#reset");
 const output = document.querySelector("#output");
+const table = document.querySelector("#table");
+const thead = document.querySelector("#thead");
+const tbody = document.querySelector("#tbody");
 const timezoneOffset = 2 * 60 * 60 * 1000;
+// (function () {
+//   let data = localStorage.getItem("shifts") ? localStorage.getItem("shifts") : [];
+//   data = JSON.parse(data);
+//   for (let item in data) {
+//     console.log(data[item]);
+//   }
+//   data = JSON.stringify(data);
+// })();
+
+//TODO
+//sort stringify/parse and add database
+//on time start fill out table for time start
+//on time end fill out table for time end
+//custom fill table
+//retroactively fill shifts
+//onShift = true/false currently working, set in iife, localStorage string. figure out better way
+//track other things i.e. reading / language learning / exercising
 
 punchIn.addEventListener("click", () => {
-  let date = new Date();
-  date.setTime(date.getTime() + timezoneOffset);
-  let dateString = date.toISOString();
-  localStorage.setItem("in", dateString);
-  console.log(localStorage.getItem("in"));
+  if (localStorage.getItem("onShift") === "true") {
+    console.log("ERROR: Already on shift");
+  } else {
+    localStorage.setItem("onShift", "true");
+    let date = new Date();
+    date.setTime(date.getTime() + timezoneOffset);
+    localStorage.setItem("shift-start", date.toISOString());
+  }
 });
 
 punchOut.addEventListener("click", () => {
-  let date = new Date();
-  date.setTime(date.getTime() + timezoneOffset);
-  let dateString = date.toISOString();
-  localStorage.setItem("out", dateString);
-  console.log(localStorage.getItem("out"));
-  console.log(endShift());
+  if (localStorage.getItem("onShift") === "false" || !localStorage.getItem("onShift")) {
+    console.log("ERROR: Not on shift");
+  } else {
+    localStorage.setItem("onShift", "false");
+    let date = new Date();
+    date.setTime(date.getTime() + timezoneOffset);
+    localStorage.setItem("shift-end", date.toISOString());
+    endShift();
+  }
+});
+
+reset.addEventListener("click", () => {
+  localStorage.removeItem("shift-start");
+  localStorage.setItem("onShift", "false");
 });
 
 function endShift() {
-  const date1String = localStorage.getItem("in");
-  const date2String = localStorage.getItem("out");
+  let date = new Date();
+  const date1String = localStorage.getItem("shift-start");
+  const date2String = localStorage.getItem("shift-end");
 
   let timeIn = new Date(date1String);
   let timeOut = new Date(date2String);
@@ -40,5 +73,23 @@ function endShift() {
   const minutesOutput = minutesDifference.toString().padStart(2, "0");
   const secondsOutput = secondsDifference.toString().padStart(2, "0");
 
-  return `${hoursOutput}:${minutesOutput}:${secondsOutput}`;
+  output.innerText = `${hoursOutput}:${minutesOutput}:${secondsOutput}`;
+
+  let obj = {
+    date: date.getDate,
+    in: timeIn.toISOString(),
+    out: timeOut.toISOString(),
+    duration: `${hoursOutput}:${minutesOutput}:${secondsOutput}`,
+  };
+  let data = localStorage.getItem("shifts");
+  data = JSON.parse(data);
+  data.push(obj);
+  data = JSON.stringify(data);
+  localStorage.setItem("shifts", data);
+  //add row
+  //add tds
+  //fill tds
+  //append tds to row
+
+  //from database, repeat above for each shift and append to table
 }
